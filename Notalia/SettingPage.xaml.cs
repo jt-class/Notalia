4dcaf074cc481eaeb51fae2fc77ba371b9a9190d
Rojas_Joseph2002 <imdreaper99@gmail.com>
2024-09-30 09:58:15 +0800

    Added Settings Page with Button

---
+++ Notalia/SettingPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System;
+using Xamarin.Forms;
+
+namespace Notalia
+{
+    public partial class SettingPage : ContentPage
+    {
+        public SettingPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnProfileClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProfilePage());
+        }
+
+        private async void OnLogOutClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Log Out", "You have been logged out.", "OK");
+        }
+
+        private async void OnAboutUsClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("About Us", "This is the About Us section.", "OK");
+        }
+
+        private async void OnHelpClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Help", "This is the Help section.", "OK");
+        }
+    }
+}
+
+
+
